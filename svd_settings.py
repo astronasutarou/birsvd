777e5b0caa20b4fca20c1a79859322b72b6e052b
Ryou Ohsawa <ohsawa@ioa.s.u-tokyo.ac.jp>
2018-11-09 00:00:52 +0900

    param: update default values of BIRSVDParameter

--- svd_settings.py
+++ svd_settings.py
@@ -6,8 +6,8 @@ import numpy as np
 
 class BIRSVDParameter(object):
   def __init__(self, n_iter=30, init_method='randOrthoNormal',
-         r_type_L = '2ndOrderDiff_acc8', r_degree_L  = 0.001,
-         r_type_R = '2ndOrderDiff_acc8', r_degree_R  = 0.001,
+         r_type_L = '2ndOrderDiff_acc8', r_degree_L  = 0.0001,
+         r_type_R = '2ndOrderDiff_acc8', r_degree_R  = 0.0001,
          lsqr_niter = 25):
     super(BIRSVDParameter,self).__init__()
     self.n_iter = n_iter
